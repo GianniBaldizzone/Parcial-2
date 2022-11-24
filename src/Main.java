31131f2aaa6c1b13c13932a6f998d0f0d3e5bcd6
escuela@davinci.edu.ar <gianni.baldizzone@davinci.edu.ar>
2022-11-24 12:58:48 -0300

    implementación de constructores propuestos en el diagrama de clases

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hola123");
+
+
         Shape s1 = new Circle("red", false, 5.5);
         Circle s2 = new Circle();
         Shape s3 = new Rectangle("red", false, 1, 2);
         Shape s4 = new Square(6.6);
+        Circle s5 = new Circle();
+        Shape s6 = new Rectangle();
 
         System.out.println(s1);
         System.out.println(s3);
         System.out.println(s4);
+        System.out.println(s5);
+        System.out.println(s6);
+
 
         s2.setColor("red");
         s2.setFilled(false);
